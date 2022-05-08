f99fdfe8e3fdde6607eebfd8fe9e0b8413bc830c
Ml06py <liaghimohamad69@gmail.com>
2022-05-08 20:09:38 +0430

    Genre & cast model added

--- apps/cast/models.py
+++ apps/cast/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=20)
+    # films = models.ForeignKey()
+
+class Cast(models.Model):
+    class role(models.TextChoices):
+        actor = "a"
+        director = "d"
+    avatar = models.ImageField(upload_to="actors/")
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    biography = models.TextField()
+    birthday  = models.DateField()
+    # works = models.ForeignKey()
+    role = models.CharField(max_length=1, choices=role.choices,default=role.actor)
+    
+
+
