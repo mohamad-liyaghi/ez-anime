57f25f67125a6caf916cee41612dd786c89101f3
Ml06py <liaghimohamad69@gmail.com>
2022-09-29 11:05:29 +0330

    Allauth login added

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,9 +1,10 @@
-from django import  forms
-from  django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
+
+class RegisterUserForm(forms.ModelForm):
+    '''A form for creating users'''
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-    
+        fields = ('email', 'password')
