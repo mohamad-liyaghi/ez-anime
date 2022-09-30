d299077d36d5c0a243cc9d991b29b2760b9d8479
Ml06py <liaghimohamad69@gmail.com>
2022-09-30 12:10:20 +0330

    Tag app added

--- apps/element/admin.py
+++ apps/element/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Genre,Cast
-# Register your models here.
+from .models import Genre, Cast, Tag
+
 admin.site.register(Genre)
 admin.site.register(Cast)
+admin.site.register(Tag)
--- apps/element/models.py
+++ apps/element/models.py
@@ -1,23 +1,34 @@
 from django.db import models
-# Create your models here.
+
 
 class Genre(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=120)
+
     def __str__(self) :
         return self.title
 
+
 class Cast(models.Model):
+
     class role(models.TextChoices):
         actor = "a"
         director = "d"
+
     avatar = models.ImageField(upload_to="actors/")
-    full_name = models.CharField(max_length=50,null=True)
+    full_name = models.CharField(max_length=50, null=True)
+
     biography = models.TextField()
     birthday  = models.DateField()
+
     role = models.CharField(max_length=1, choices=role.choices,default=role.actor)
     token = models.CharField(max_length=10,blank=True)
+
     def __str__(self):
         return self.full_name
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=120)
         
-
-
+    def __str__(self):
+        return self.title
