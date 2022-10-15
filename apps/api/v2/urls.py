4452eee48bad3bcae682bdbbcaa4686bdd6533a3
Ml06py <liaghimohamad69@gmail.com>
2022-10-15 12:33:29 +0330

    Film Viewset  added

--- apps/api/v2/urls.py
+++ apps/api/v2/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include
+from rest_framework_nested import routers
+from . import views
 
 app_name = 'v2'
+
+
+router = routers.DefaultRouter()
+
+router.register('film', views.FilmViewSet, basename="film")
+
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += router.urls
---
+++ apps/api/v2/views.py
@@ -0,0 +1,6 @@
+from rest_framework.viewsets import ModelViewSet
+
+
+class FilmViewSet(ModelViewSet):
+    '''A Viewset for Get/Create/Update/Detail a movie'''
+    pass
